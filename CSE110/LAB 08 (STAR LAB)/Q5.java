252c42b9e8684240c4d07e0111ea09e4a81d0b58
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2019-06-26 04:45:41 +0600

    Add files via upload

---
+++ CSE110/LAB 08 (STAR LAB)/Q5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Q5{
+public static void main (String args[]){
+  Scanner input = new Scanner(System.in);
+  int row = input.nextInt();
+  for(int i=1;i<=row;i++){
+    for(int j=1;j<=i;j++){
+      System.out.print("*");
+  }
+    System.out.println();
+  }
+}
+}
