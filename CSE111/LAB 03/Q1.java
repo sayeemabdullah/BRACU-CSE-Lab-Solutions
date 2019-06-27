e4c5c50dbc8e25a4109a772d5be0193b5263fde8
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2019-06-28 02:19:11 +0600

    Add files via upload

---
+++ CSE111/LAB 03/Q1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Q1{
+public static void main (String args[]){
+  Scanner input = new Scanner(System.in);
+  String str = input.next();
+  System.out.println(str.length());
+}
+}
