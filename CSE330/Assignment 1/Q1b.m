0098a6a9c42815b0b95601b6c4d7d5ec9ca06a04
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2019-07-05 18:35:00 +0600

    Add files via upload

---
+++ CSE330/Assignment 1/Q1b.m
@@ -0,0 +1,13 @@
+clc;
+A=1;
+B=7;
+C=1;
+D=0;
+E=1;
+F=0;
+G=0;
+H=9;
+M=4;
+
+ans = (3*(A*(C^2))) - ceil(B/pi) + floor(sqrt((16*B)-(D*H))) ;
+disp(ans);
---
+++ CSE330/Assignment 1/Q1c.m
@@ -0,0 +1,13 @@
+clc;
+A=1;
+B=7;
+C=1;
+D=0;
+E=1;
+F=0;
+G=0;
+H=9;
+M=4;
+
+ans = (M/C) + ((2*pi)*floor(((M^2)*C)/B)) - abs((G*H)-(B*M)) ;
+disp(ans);
