ad26049b34dac04b174ed11a74f760fd401007e3
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2019-11-05 22:15:40 +0600

    Add files via upload

---
+++ CSE391/Assignment 03/comp207.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2019 at 05:09 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `comp207`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slots`
+--
+
+CREATE TABLE `slots` (
+  `slot` varchar(255) NOT NULL,
+  `seats` int(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `slots`
+--
+
+INSERT INTO `slots` (`slot`, `seats`) VALUES
+('1', 39),
+('2', 37),
+('3', 39),
+('4', 40);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `SID` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `slot` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`fname`, `lname`, `SID`, `email`, `slot`) VALUES
+('s', 's', '123', '1@gmail.com', '2'),
+('s', 'Abdullah', '17101008', 'abir@gmail.com', '1'),
+('Sayeem', 'Abdullah', '17101009', 'sayeem.mdabdullah@gmail.com', '3');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `slots`
+--
+ALTER TABLE `slots`
+  ADD PRIMARY KEY (`slot`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`SID`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `slot` (`slot`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`slot`) REFERENCES `slots` (`slot`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
